34aa75f253ead867f945f3951c40033c9447009b
shkarov <37274759+shkarov@users.noreply.github.com>
2018-11-26 21:54:34 +0300

    Update Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -1,14 +1,11 @@
 <?php
 namespace BrainGames\Cli;
 
-use function \cli\line;
-use function \cli\prompt;
-
 function run()
 {
-line('Welcome to the Brain Games!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
-return true;
+  \cli\line('Welcome to the Brain Games!');
+  $name = \cli\prompt('May I have your name?');
+  \cli\line("Hello, %s!", $name);
+  return true;
 }
 
