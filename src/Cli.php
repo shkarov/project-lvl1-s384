f9a80dcfb746c9252f2ff0f9bb3c141d695b6298
Boris Shkarov <b.shkarov@gmail.com>
2018-11-27 17:11:24 +0300

    update json and Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -3,9 +3,8 @@ namespace BrainGames\Cli;
 
 function run()
 {
-  \cli\line('Welcome to the Brain Games!');
-  $name = \cli\prompt('May I have your name?');
-  \cli\line("Hello, %s!", $name);
-  return true;
+    \cli\line('Welcome to the Brain Games!');
+    $name = \cli\prompt('May I have your name?');
+    \cli\line("Hello, %s!", $name);
+    return true;
 }
-
