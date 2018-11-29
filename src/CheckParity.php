7b3eafd9b37cb01dca6f321625716adc0d923a1c
Boris Shkarov <b.shkarov@gmail.com>
2018-11-29 16:25:12 +0300

    update json and checkparity.php

--- src/CheckParity.php
+++ src/CheckParity.php
@@ -1,26 +1,22 @@
 <?php
 namespace BrainGames\CheckParity;
 
-private function isParity(integer $number)
+function isParity($number)
 {
     return ($number % 2 == 0) ? true : false;
 }
-
 function check()
 {
     \cli\line('Welcome to the Brain Games!');
     \cli\line('Answer "yes" if number even otherwise answer "no".');
-    echo "\n";
     $name = \cli\prompt('May I have your name?');
     \cli\line("Hello, %s!", $name);
-    echo "\n";
-    
-    $countCorrect = 0
+    $countCorrect = 0;
     for ($i = 0; $i < 3; $i++) {
         $randomNumber = rand(1, 100);
-        \cli\line("Question:, %i", $randomNumber);
-        $answerNumber = \cli\prompt('Your answer:');
-        if (isParity($randomNumber) == isParity($answerNumber)) {
+        \cli\line("Question:, %s", $randomNumber);
+        $answer = \cli\prompt('Your answer');
+        if ((isParity($randomNumber) && strtolower($answer) === "yes") || (!isParity($randomNumber) && strtolower($answer) === "no")) {
             $countCorrect += 1;
             \cli\line('Correct!');
         } else {
