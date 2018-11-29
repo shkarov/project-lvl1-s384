57eca80e46291bcccb8096a89ee25ae786988eaa
Boris Shkarov <b.shkarov@gmail.com>
2018-11-30 00:26:42 +0300

    update step 4

--- src/Calc.php
+++ src/Calc.php
@@ -1,33 +1,34 @@
 <?php
 namespace BrainGames\Calc;
 
+function arithmetic($num1, $num2, $sign)
+{
+    switch ($sign) {
+        case "+":
+            return $num1 + $num2;
+        case "-":
+            return $num1 - $num2;
+        case "*":
+            return $num1 * $num2;                
+    }
+}
 function calc()
 {
     \cli\line('Welcome to the Brain Games!');
     \cli\line('What is the result of the expression?');
     $name = \cli\prompt('May I have your name?');
     \cli\line("Hello, %s!", $name);
-
-    $arithSigns = array('+', '-', '*');
-
+    $arrSigns = array('+', '-', '*');
     $countCorrect = 0;
-    //for ($i = 0; $i < 3; $i++) {
-
+    for ($i = 0; $i < 3; $i++) {
         $rNum1 = rand(1, 100);
         $rNum2 = rand(1, 100);
-        $signIndex = array_rand($arithSigns);
-        $sign = $arithSigns[$signIndex];
-
+        $signIndex = array_rand($arrSigns);
+        $sign = $arrSigns[$signIndex];
         \cli\line("Question:, %s", "{$rNum1} {$sign} {$rNum2}");
-        //$answer = \cli\prompt('Your answer');
-
-        $result = "{$rNum1} {$sign} {$rNum2}";
-        var_dump($result);
-        $x = eval($result);
-        
-        var_dump($x);
-
-    /*    if ((isParity($rNum) && strtolower($answer) == "yes") || (!isParity($rNum) && strtolower($answer) == "no")) {
+        $result = arithmetic($rNum1, $rNum2, $sign);
+        $answer = \cli\prompt('Your answer');
+        if ($result == $answer) {
             $countCorrect += 1;
             \cli\line('Correct!');
         } else {
@@ -35,8 +36,7 @@ function calc()
             \cli\line("Let's try again, %s!", $name);
             break;
         }
-    */    
-    //}
+    }
     if ($countCorrect == 3) {
         \cli\line("Congratulations, %s!", $name);
     }
