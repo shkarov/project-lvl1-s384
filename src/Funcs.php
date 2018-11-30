226ba80c96406e9e6f6b12400f651a37ffd3fd4c
Boris Shkarov <b.shkarov@gmail.com>
2018-11-30 14:48:52 +0300

    update all step 6

--- src/Funcs.php
+++ src/Funcs.php
@@ -33,3 +33,14 @@ function arithmetic($num1, $num2, $sign)
             return $num1 * $num2;
     }
 }
+function algEuclidian($num1, $num2)
+{
+    while ($num1 !== $num2) {
+        if ($num1 > $num2) {
+            $num1 = $num1 - $num2;
+        } else {
+            $num2 = $num2 - $num1;
+        }
+    }
+    return $num1;
+}
